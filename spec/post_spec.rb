d917a02e61af2c473d769535da30211591137934
tracey brimage <onelovenilynn@gmail.com>
2019-10-15 06:33:57 +0000

    Automatically backed up by Learn

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,19 +1,30 @@
 require "spec_helper"
 
 describe "Post" do
-  let!(:post) { Post.new } 
+  let!(:post) { Post.new }
   it "has a title" do
-    post.title = "Hello World" 
-    expect(post.title).to eq("Hello World")
+    class author
+
+      attr_accesor :name
+
+      def initialize (name)
+
+        @ name=name
+      end
+    end
+    Post= post.new
+    author.name = "Uncle Bob"
+    post.author.name
+
   end
 
-  it "belongs to an author" do 
+  it "belongs to an author" do
     author = Author.new
     post.author = author
     expect(post.author).to eq(author)
   end
 
-  it "knows the name of the author it belongs to" do 
+  it "knows the name of the author it belongs to" do
     author = Author.new
     author.name = "Uncle Bob"
     post.author = author
