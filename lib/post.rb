f1b264cec2d2b68e40937348d5cf6c4353bf6615
tracey brimage <onelovenilynn@gmail.com>
2019-10-15 06:36:48 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,12 +1,16 @@
 class post
 
-  attr_accesor :title
+  attr_accesor :title,:author
 
-  def initialize (title)
+  def initialize (title,author)
 
     @ title=title
+    @author=author
   end
 end
 post = post.new
 post.name = "hello World"
 post.author.name
+
+author=author.new
+author.name=
